db2d6bb55cb4c85817ffea032b53928a8b59a0e1
Patrik <madarpatrik369@gmail.com>
2026-07-24 01:05:51 +0200

    update
    update

--- img/arena_colors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="arena_colors" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="arena_colors.png" width="128" height="128"/>
-</tileset>
--- img/marble.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="marble" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="greek_tiles.png" width="64" height="64"/>
-</tileset>
